7e5ff0e3ed4c54a6c249f307c4de36a1f9325ca1
Bora Kaya <kayabo19@itu.edu.tr>
2025-01-14 13:42:31 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -1210,6 +1210,7 @@ if page=="Özel Kapsamlı Göstergeler":
             name="Mevsimsellikten Arındırılmış",
             marker=dict(color='blue'),
             text=[f"{val:.2f}%" for val in gösterge_artıs_ma],  # Çubuğun üstüne değer ekleme
+            textfont=dict(size=20, family="Arial", color="black"),
             textposition='outside',
         ))
 
@@ -1221,6 +1222,7 @@ if page=="Özel Kapsamlı Göstergeler":
             name="Ham Veriler",
             marker=dict(color='orange'),
             text=[f"{val:.2f}%" for val in gösterge_artıs_ham],  # Çubuğun üstüne değer ekleme
+            textfont=dict(size=20, family="Arial", color="black"),
             textposition='outside'
         ))
 

db4f81529218f41187fe81c1f252ef9dd7c82fdb
Bora Kaya <kayabo19@itu.edu.tr>
2025-01-16 13:24:17 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -1241,7 +1241,8 @@ if page=="Mevsimsellikten Arındırılmış Göstergeler":
             "inside" if val>(max_ma-2) or val <( min_ma+2) else "outside"
             for val in gösterge_artıs_ma.values
         ],
-        textfont=dict(size=14, family="Arial Black", color="black")
+        textfont=dict(size=14, family="Arial Black", color="black"),
+        insidetextfont=dict(size=14, family="Arial Black", color="black")
     ))
 
     # Ham Veriler

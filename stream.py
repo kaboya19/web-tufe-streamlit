53616cbb51d0543fafa014b1074e8023e7e8574e
Bora Kaya <kayabo19@itu.edu.tr>
2025-03-03 11:53:25 +0300

    .

--- stream.py
+++ stream.py
@@ -555,6 +555,30 @@ if page=="Tüketici Fiyat Endeksi":
         st.plotly_chart(figgalt)
         st.markdown(f"<h2 style='text-align:left; color:black;'>{selected_group} Aylık Artış Oranı</h2>", unsafe_allow_html=True)
         st.plotly_chart(figgartıs)
+
+        figcomp=go.Figure()
+        tüik_aylık=tüik["TÜİK"].pct_change().dropna().iloc[1:]*100
+        tüik_aylık.index=tüik_aylık.index.strftime("%Y-%m")
+        cari=hareketli_aylik_ortalama(tüfe.iloc[:,0])["Aylık Ortalama"].fillna(method="ffill")
+        tüfeaylıkdata=cari.resample('M').last().pct_change().loc["2025-02":]*100
+        tüfeaylıkdata.iloc[-1]=hareketliartıs.iloc[-1]
+        tüfeaylıkdata=pd.DataFrame(tüfeaylıkdata)
+        tüfeaylıkdata.columns=["Aylık Artış"]
+        tüfeaylıkdata["Tarih"]=pd.to_datetime(tüfeaylıkdata.index)
+        tüfeaylıkdata["Tarih"]=tüfeaylıkdata["Tarih"].dt.strftime("%Y-%m")
+        tüfeaylıkdata=tüfeaylıkdata.set_index("Tarih")
+        tüfeaylıkdata["TÜİK"]=tüik_aylık["TÜİK"]
+        
+
+
+
+
+
+
+
+
+
+
         gruplar24=pd.read_csv("gruplar24.csv",index_col=0)
         gruplar=pd.read_csv("anagruplar.csv",index_col=0)
         gruplar.index=pd.to_datetime(gruplar.index)

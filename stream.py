17834a4a5a6c817591ad3ad0fa028317c7ee6628
Bora Kaya <kayabo19@itu.edu.tr>
2025-04-05 17:39:24 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -64,7 +64,7 @@ secim = st.selectbox("Veri türünü seçin:", ["Madde", "Harcama Grubu"], key="
 if secim == "Madde":
     df = pd.read_csv("endeksler.csv", index_col=0)
 else:
-    df = pd.read_csv("harcamagruplari.csv", index_col=0)
+    df = pd.read_csv("harcama_grupları.csv", index_col=0)
 
 # ---------------- Günlük Değişim Hesapla ----------------
 degisimler = df.pct_change().dropna().iloc[-1].sort_values(ascending=False) * 100
@@ -89,7 +89,7 @@ kayan_yazi_box.markdown(f"""
         .kayan_yazi {{
             display: inline-block;
             white-space: nowrap;  /* Tek satırda göster */
-            animation: scroll-left 240s linear infinite;
+            animation: scroll-left 300s linear infinite;
         }}
         
         @keyframes scroll-left {{

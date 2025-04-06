1f08d6ee8d0ef0b6654c9aedea66f2a3eff4fa5e
Bora Kaya <kayabo19@itu.edu.tr>
2025-04-06 12:19:53 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -93,7 +93,7 @@ streamlit_marquee(
     fontSize="18px",
     background="#ffffff",
     color="#000000",
-    animationDuration=3000,
+    animationDuration=30000,
     lineHeight="30px",
     width="100%"
 )

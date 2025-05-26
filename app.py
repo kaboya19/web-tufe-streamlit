1f7b0ed358f5936610c7f32cdc1bbe1f38b9595a
Bora Kaya <kayabo19@itu.edu.tr>
2025-05-26 12:36:34 +0300

    .

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-import pandas as pd
-import plotly.express as px
+from playwright.sync_api import sync_playwright
 
-# CSV verisini oku
-url = "https://raw.githubusercontent.com/kaboya19/web-tufe-streamlit/main/t%C3%BCfe.csv"
-df = pd.read_csv(url)
-df.columns = [col.strip() for col in df.columns]
-df[df.columns[0]] = pd.to_datetime(df[df.columns[0]], dayfirst=True)
-
-# Plotly grafik oluştur
-fig = px.line(df, x=df.columns[0], y=df.columns[1], title="TÜFE Zaman Serisi")
-
-# HTML olarak kaydet
-fig.write_html("tufe_plot.html")
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto("https://www.hepsiemlak.com/konyaalti-kiralik-sahibinden")
+    page.wait_for_timeout(3000)  # Sayfanın yüklenmesini bekleyin
+    content = page.content()
+    print(content)
+    browser.close()

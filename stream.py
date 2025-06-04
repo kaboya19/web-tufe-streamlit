ed1a83946352e46ae9682ab0c765321977d41562
Bora Kaya <kayabo19@itu.edu.tr>
2025-06-04 09:46:05 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.markdown("""
-    <meta http-equiv="refresh" content="0; url='https://www.webtufe.com'" />
+    <meta http-equiv="refresh" content="0; url='https://receiver-five.vercel.app/'" />
 """, unsafe_allow_html=True)
